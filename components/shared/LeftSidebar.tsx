35942604ad49cdd5c554ad7064c7f484d8f729a3
DrrylPns <darrylpaniss@gmail.com>
2023-09-06 04:37:12 +0800

    third commit

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs"
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs"
 import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,12 +8,15 @@ import { usePathname, useRouter } from "next/navigation";
 function LeftSidebar() {
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
 
     return (
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
                 {sidebarLinks.map((link) => {
                     const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
+
+                    if (link.route === '/profile') link.route = `${link.route}/${userId}`
 
                 return(
                     <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}> 
